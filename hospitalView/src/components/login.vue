98ad1abc3c71be40c4154f264bf75cec2c29a917
Lu Boyan <376597962@qq.com>
2021-07-11 15:05:28 +0800

    更新了登录和注册界面

--- hospitalView/src/components/login.vue
+++ hospitalView/src/components/login.vue
@@ -1,127 +1,264 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="avatar_box">
-                 <img src="../assets/avatar.jpg" alt="">
-            </div>
-            <!-- 登录表单区域 -->
-        <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="0px" class="login_form">
-            <!-- 用户名 -->
-             <el-form-item prop="username">
-             <el-input v-model="loginForm.username"   prefix-icon="el-icon-user-solid"></el-input>
-            </el-form-item>
-            <!-- 密码 -->
-             <el-form-item prop="password">
-             <el-input type="password" prefix-icon="el-icon-edit" v-model="loginForm.password"></el-input>
-             </el-form-item >
-            <!-- 按钮 -->
-            <el-form-item class="btns">
-                <el-button type="primary" @click="login">登录</el-button>
-                <el-button type="info" @click="resetLoginForm">重置</el-button>
-            </el-form-item>
-        </el-form>
-        </div>
-    </div>
+	<div class="login-register">
+		<div class="contain">
+			<div class="big-box" :class="{active:isLogin}">
+				<div class="big-contain" v-if="isLogin">
+					<div class="btitle">账户登录</div>
+					<div class="bform">
+						<input type="email" placeholder="邮箱" v-model="form.useremail">
+						<span class="errTips" v-if="emailError">* 邮箱填写错误 *</span>
+						<input type="password" placeholder="密码" v-model="form.userpwd">
+						<span class="errTips" v-if="emailError">* 密码填写错误 *</span>
+					</div>
+					<button class="bbutton" @click="login">登录</button>
+				</div>
+				<div class="big-contain" v-else>
+					<div class="btitle">创建账户</div>
+					<div class="bform">
+						<input type="text" placeholder="用户名" v-model="form.username">
+						<span class="errTips" v-if="existed">* 用户名已经存在！ *</span>
+						<input type="email" placeholder="邮箱" v-model="form.useremail">
+						<input type="password" placeholder="密码" v-model="form.userpwd">
+					</div>
+					<button class="bbutton" @click="register">注册</button>
+				</div>
+			</div>
+			<div class="small-box" :class="{active:isLogin}">
+				<div class="small-contain" v-if="isLogin">
+					<div class="stitle">你好!</div>
+					<p class="scontent">开始注册，进入医院管理系统</p>
+					<button class="sbutton" @click="changeType">注册</button>
+				</div>
+				<div class="small-contain" v-else>
+					<div class="stitle">医院管理系统</div>
+					<p class="scontent">与我们保持联系，请登录你的账户</p>
+					<button class="sbutton" @click="changeType">登录</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-    data(){
-        return{
-            loginForm:{
-                username:'院长',
-                password:'123456'
-            },
-             loginRules: {
-                username: [
-                    { min:3, max:10, message:"长度需要在3到10个字符之间" },
-                    { required: true, message: '请输入用户名', trigger: 'blur' }
-                ],
-                password: [
-                    { min:6, max:15, message:"长度需要在6到15个字符之间" },
-                    { required: true, message: '请输入密码', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-
-    methods:{
-        resetLoginForm(){
-            //console.log(this);
-            this.$refs.loginFormRef.resetFields();this.$refs.loginFormRef.resetFields();
-            return this.$message.success('你成功重置了登录信息！');
-        },
-        login(){
-            /*
-                登录请求：先判断valid是否为true
-                如果为true才发起登录请求，再配置axios登陆请求
-                请求地址在API接口文档中，轻轻参数也必须为后端提供好
-                post返回的值是一个promise，为了简化promise操作，用await和async优化，这样就可以直接拿到服务器返回的数据
-
-            */
-            this.$refs.loginFormRef.validate(async valid=>{
-                //console.log(valid);
-                if(!valid) return;
-                //重命名，将返回的data属性重命名为res
-                const {data:res}=await this.$http.post('login',this.loginForm);
-                //console.log(res);
-                if(res.meta.status!=200) {
-                    //return console.log('登录失败')
-                    return this.$message.console.error('登录失败')
-                }
-                   
-                this.$message.console.success('登录成功')
-            });
-            this.$router.push("/main_home");
-        }
-    }
-};
+	export default{
+		name:'login-register',
+		data(){
+			return {
+				isLogin:false,
+				emailError: false,
+				passwordError: false,
+				existed: false,
+				form:{
+					username:'',
+					useremail:'',
+					userpwd:''
+				}
+			}
+		},
+		methods:{
+			changeType() {
+				this.isLogin = !this.isLogin
+				this.form.username = ''
+				this.form.useremail = ''
+				this.form.userpwd = ''
+			},
+			login() {
+				const self = this;
+				if (self.form.useremail != "" && self.form.userpwd != "") {
+					self.$axios({
+						method:'post',
+						url: 'http://127.0.0.1:10520/api/user/login',
+						data: {
+							email: self.form.useremail,
+							password: self.form.userpwd
+						}
+					})
+					.then( res => {
+						switch(res.data){
+							case 0: 
+								alert("登录成功！");
+								break;
+							case -1:
+								this.emailError = true;
+								break;
+							case 1:
+								this.passwordError = true;
+								break;
+						}
+					})
+					.catch( err => {
+						console.log(err);
+					})
+				} else{
+					alert("填写不能为空！");
+				}
+			},
+			register(){
+				const self = this;
+				if(self.form.username != "" && self.form.useremail != "" && self.form.userpwd != ""){
+					self.$axios({
+						method:'post',
+						url: 'http://127.0.0.1:10520/api/user/add',
+						data: {
+							username: self.form.username,
+							email: self.form.useremail,
+							password: self.form.userpwd
+						}
+					})
+					.then( res => {
+						switch(res.data){
+							case 0:
+								alert("注册成功！");
+								this.login();
+								break;
+							case -1:
+								this.existed = true;
+								break;
+						}
+					})
+					.catch( err => {
+						console.log(err);
+					})
+				} else {
+					alert("填写不能为空！");
+				}
+			}
+		}
+	}
 </script>
 
-<style lang="less" scoped>
-.login_container {
-    background-color: #2b4b6b;
-    height:100%;
-}
-.login_box{
-    width:450px;
-    height:300px;
-    background-color: aliceblue;
-    border-radius: 3px;
-    position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
-
-    .avatar_box{
-        height:130px;
-        width:130px;
-        border:1px solid #fff;
-        border-radius: 50%;
-        padding: 10px;
-        box-shadow: 0 0 10px #ddd;
-        position: absolute;
-        left:50%;
-        transform :translate(-50%,-50%);
-        background-color: #fff;
-        img{
-            height: 100%;
-            width: 100%;
-            border-radius: 50%;
-        }
-    }
-}
-.login_form{
-    position: absolute;
-    bottom: 0;
-    width:100%;
-    padding:0 20px;
-    box-sizing: border-box;
-}
-.btns{
-    display: flex;
-    justify-content: flex-end;
-}
+<style scoped="scoped">
+	.login-register{
+		width: 100vw;
+		height: 100vh;
+		box-sizing: border-box;
+	}
+	.contain{
+		width: 60%;
+		height: 60%;
+		position: relative;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		background-color: #fff;
+		border-radius: 20px;
+		box-shadow: 0 0 3px #f0f0f0,
+					0 0 6px #f0f0f0;
+	}
+	.big-box{
+		width: 70%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 30%;
+		transform: translateX(0%);
+		transition: all 1s;
+	}
+	.big-contain{
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.btitle{
+		font-size: 1.5em;
+		font-weight: bold;
+		color: rgb(57,167,176);
+	}
+	.bform{
+		width: 100%;
+		height: 40%;
+		padding: 2em 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.bform .errTips{
+		display: block;
+		width: 50%;
+		text-align: left;
+		color: red;
+		font-size: 0.7em;
+		margin-left: 1em;
+	}
+	.bform input{
+		width: 50%;
+		height: 30px;
+		border: none;
+		outline: none;
+		border-radius: 10px;
+		padding-left: 2em;
+		background-color: #f0f0f0;
+	}
+	.bbutton{
+		width: 20%;
+		height: 40px;
+		border-radius: 24px;
+		border: none;
+		outline: none;
+		background-color: rgb(0, 81, 255);
+		color: #fff;
+		font-size: 0.9em;
+		cursor: pointer;
+	}
+	.small-box{
+		width: 30%;
+		height: 100%;
+		background: linear-gradient(135deg,rgb(1, 153, 255),rgb(0, 88, 252));
+		position: absolute;
+		top: 0;
+		left: 0;
+		transform: translateX(0%);
+		transition: all 1s;
+		border-top-left-radius: inherit;
+		border-bottom-left-radius: inherit;
+	}
+	.small-contain{
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.stitle{
+		font-size: 1.5em;
+		font-weight: bold;
+		color: #fff;
+	}
+	.scontent{
+		font-size: 0.8em;
+		color: #fff;
+		text-align: center;
+		padding: 2em 4em;
+		line-height: 1.7em;
+	}
+	.sbutton{
+		width: 60%;
+		height: 40px;
+		border-radius: 24px;
+		border: 1px solid #fff;
+		outline: none;
+		background-color: transparent;
+		color: #fff;
+		font-size: 0.9em;
+		cursor: pointer;
+	}
+	
+	.big-box.active{
+		left: 0;
+		transition: all 0.5s;
+	}
+	.small-box.active{
+		left: 100%;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+		border-top-right-radius: inherit;
+		border-bottom-right-radius: inherit;
+		transform: translateX(-100%);
+		transition: all 1s;
+	}
 </style>
-
