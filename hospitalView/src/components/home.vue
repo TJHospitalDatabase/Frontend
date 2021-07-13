8c1d813f08455d1f70e2657de59c7f30e078268e
yifan <1042750083@qq.com>
2021-07-14 02:48:22 +0800

    跨域传参

--- hospitalView/src/components/home.vue
+++ hospitalView/src/components/home.vue
@@ -27,48 +27,48 @@
                 <template slot="title" v-if="(tokenStr-0)&7"><i class="el-icon-info"></i>门诊管理</template>
                 <el-menu-item-group>
                     <template slot="title" v-if="(tokenStr-0)&2">挂号信息</template>
-                    <el-menu-item index="registerpage11" v-if="(tokenStr-0)&2">病人信息录入</el-menu-item>
-                    <el-menu-item index="registerpage12" v-if="(tokenStr-0)&2">病人信息查询</el-menu-item>
-                    <el-menu-item index="registerpage13" v-if="(tokenStr-0)&2">挂号</el-menu-item>
-                    <el-menu-item index="registerpage14" v-if="(tokenStr-0)&2">挂号单处理</el-menu-item>
+                    <el-menu-item index :route='{path:"registerpage11", query:{id:id,name:name}}' v-if="(tokenStr-0)&2">病人信息录入</el-menu-item>
+                    <el-menu-item index :route='{path:"registerpage12", query:{id:id,name:name}}' v-if="(tokenStr-0)&2">病人信息查询</el-menu-item>
+                    <el-menu-item index :route='{path:"registerpage13", query:{id:id,name:name}}' v-if="(tokenStr-0)&2">挂号</el-menu-item>
+                    <el-menu-item index :route='{path:"registerpage14", query:{id:id,name:name}}' v-if="(tokenStr-0)&2">挂号单处理</el-menu-item>
                 </el-menu-item-group>
                 <el-menu-item-group>
                     <template slot="title" v-if="(tokenStr-0)&4">查询挂号</template>
-                    <el-menu-item index="registerpage2" v-if="(tokenStr-0)&4">挂号信息查询</el-menu-item>
+                    <el-menu-item index :route='{path:"registerpage2", query:{id:id,name:name}}' v-if="(tokenStr-0)&4">挂号信息查询</el-menu-item>
                 </el-menu-item-group>
                 <el-menu-item-group>
                     <template slot="title" v-if="(tokenStr-0)&1">问诊信息</template>
-                    <el-menu-item index="treated" v-if="(tokenStr-0)&1">问诊信息列表</el-menu-item>
+                    <el-menu-item index :route='{path:"treated", query:{id:id,name:name}}' v-if="(tokenStr-0)&1">问诊信息列表</el-menu-item>
                 </el-menu-item-group>
             </el-submenu>
 
             <el-submenu index="2" v-if="(tokenStr-0)&8">
                 <template slot="title" v-if="(tokenStr-0)&8"><i class="el-icon-info"></i>药品管理</template>
-                <el-menu-item index="drugStore" v-if="(tokenStr-0)&8">药品库管理</el-menu-item>
-                <el-menu-item index="prescribe" v-if="(tokenStr-0)&8">开药查询</el-menu-item>
+                <el-menu-item index :route='{path:"drugStore", query:{id:id,name:name}}' v-if="(tokenStr-0)&8">药品库管理</el-menu-item>
+                <el-menu-item index :route='{path:"prescribe", query:{id:id,name:name}}' v-if="(tokenStr-0)&8">开药查询</el-menu-item>
             </el-submenu>
 
             <el-submenu index="3" v-if="(tokenStr-0)&20">
                 <template slot="title" v-if="(tokenStr-0)&20"><i class="el-icon-info"></i>单目管理</template>
-                <el-menu-item index="Checklist" v-if="(tokenStr-0)&16">检查单管理</el-menu-item>
-                <el-menu-item index="Checklist_result" v-if="(tokenStr-0)&16">检查单结果管理</el-menu-item>
-                <el-menu-item index="prescription" v-if="(tokenStr-0)&4">处方单开具</el-menu-item>
-                <el-menu-item index="examine" v-if="(tokenStr-0)&4">检查单开具</el-menu-item>
-                <el-menu-item index="hospital" v-if="(tokenStr-0)&4">住院单开具</el-menu-item>
+                <el-menu-item index :route='{path:"Checklist", query:{id:id,name:name}}' v-if="(tokenStr-0)&16">检查单管理</el-menu-item>
+                <el-menu-item index :route='{path:"Checklist_result", query:{id:id,name:name}}' v-if="(tokenStr-0)&16">检查单结果管理</el-menu-item>
+                <el-menu-item index :route='{path:"prescription", query:{id:id,name:name}}' v-if="(tokenStr-0)&4">处方单开具</el-menu-item>
+                <el-menu-item index :route='{path:"examine", query:{id:id,name:name}}' v-if="(tokenStr-0)&4">检查单开具</el-menu-item>
+                <el-menu-item index :route='{path:"hospital", query:{id:id,name:name}}' v-if="(tokenStr-0)&4">住院单开具</el-menu-item>
             </el-submenu>
 
             <el-submenu index="4" v-if="(tokenStr-0)&32">
                 <template slot="title" v-if="(tokenStr-0)&32"><i class="el-icon-info"></i>住院管理</template>
-                <el-menu-item index="searchPatient" v-if="(tokenStr-0)&32">住院病人信息查询</el-menu-item>
+                <el-menu-item index :route='{path:"searchPatient", query:{id:id,name:name}}' v-if="(tokenStr-0)&32">住院病人信息查询</el-menu-item>
                 <!-- <el-menu-item index="1-1" >通过姓名查询</el-menu-item>
                 <el-menu-item index="1-2" >通过房间号查询</el-menu-item>
                 <el-menu-item index="1-3" >通过医生查询</el-menu-item>
                 <el-menu-item index="1-4" >通过负责护士查询</el-menu-item> -->
-                <el-menu-item index="searchRoom" v-if="(tokenStr-0)&32">病房信息查询</el-menu-item>
+                <el-menu-item index :route='{path:"searchRoom", query:{id:id,name:name}}' v-if="(tokenStr-0)&32">病房信息查询</el-menu-item>
                 <!-- <el-menu-item index="2-1" >通过科室查询</el-menu-item>
                 <el-menu-item index="2-3" >通过负责护士查询</el-menu-item>
                 <el-menu-item index="2-4" >通过日均费用查询</el-menu-item> -->
-                <el-menu-item index="nurse_on_duty" v-if="(tokenStr-0)&32">住院护士值班信息</el-menu-item>
+                <el-menu-item index :route='{path:"nurse_on_duty", query:{id:id,name:name}}' v-if="(tokenStr-0)&32">住院护士值班信息</el-menu-item>
             </el-submenu>
 
 
@@ -134,6 +134,12 @@ font-size: 13px;
 
 <script>
 export default{
+  data(){
+    return{
+      id:'233333',
+      name:'怎么还有人在做数据库啊医生'
+    }
+  },
   computed:{
     tokenStr:()=>{ return window.sessionStorage.getItem('token')}
   },
