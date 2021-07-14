d800cec941616fa023a556e9f63f91b7b6616bba
YangJing <1854025@tongji.edu.cn>
2021-07-14 19:46:20 +0800

    Update registerpage2.vue

--- hospitalView/src/components/register/registerpage2.vue
+++ hospitalView/src/components/register/registerpage2.vue
@@ -1,15 +1,9 @@
+                                            
 <template>
 <el-container style="height: 500px; height:100%; border: 1px solid #eee">
-    <el-main>
-    <el-form ref="search11Ref" :model="departmentIDSearch" :rules="searchRules" label-width="0px" class="search_form">
-                <!-- 搜索框 -->
-                    <el-form-item prop="name">
-                    <el-input v-model="departmentIDSearch.DEPT_NAME"   prefix-icon="el-icon-zoom-in" style="width:70%;"></el-input>
-                    <el-button type="primary" @click="search" style="margin-left:20px;">搜索</el-button>          
-                    </el-form-item>
-                </el-form>
+    <el-main>   
   <el-table
-    :data="patientData"
+    :data="patientData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
     style="width: 100%">
 
     <el-table-column prop="registratioN_ID"
@@ -49,6 +43,16 @@
       </template>
     </el-table-column>
   </el-table>
+        <div class="block" style="margin-top:15px;">
+          <el-pagination align='center' @size-change="handleSizeChange" @current-change="handleCurrentChange" 
+            :current-page="currentPage" 
+            :page-sizes="[1,2,5,10,20]" 
+            :page-size="pageSize" 
+            layout="total, sizes, prev, pager, next, jumper" 
+            :total="patientData.length">
+          </el-pagination>
+        </div>
+
 
     </el-main>
 </el-container>
@@ -62,6 +66,9 @@ const axios = require('axios');
   export default {
     data() {
       return {
+        currentPage: 1, // 当前页码
+        total: 20, // 总条数
+        pageSize: 5, // 每页的数据条数
         departmentIDSearch:{
               DEPT_NAME:''
             },
@@ -82,21 +89,35 @@ const axios = require('axios');
         }
       };
     },
+    created(){
+      this.search()
+    },
     methods: {
         async search(){
-          const { data: res } =await this.$http.get('registration', { params: { DEPT_NAME: this.departmentIDSearch.DEPT_NAME}})
+          const { data: res } =await this.$http.get('registration', { params: { DEPT_NAME: this.$route.query.dept_name}})
             console.log(res.data)
             // 将data属性重命名为res
             this.patientData=res.data
             // console.log(this.patientData)
             },
+        //每页条数改变时触发 选择一页显示多少行
+        handleSizeChange(val) {
+            //console.log(`每页 ${val} 条`);
+            this.currentPage = 1;
+            this.pageSize = val;
+        },
+        //当前页改变时触发 跳转其他页
+        handleCurrentChange(val) {
+            //console.log(`当前页: ${val}`);
+            this.currentPage = val;
+        },
         async select_this(index, row) {
           console.log(index, row);
           this.temp.registratioN_ID=row.registratioN_ID
           const { data: res } = await this.$http.post('registration',
                 {
                   REGISTRATION_ID: this.temp.registratioN_ID,
-                  DOCTOR_ID: "ls"
+                  DOCTOR_ID: this.$route.query.id
                   }
                 )
           console.log(res.data[0])
